d2c917afcae0a3db903afc99f41e3c71aa941155
blacktyger <blacktyg3r@gmail.com>
2022-10-20 19:06:15 +0100

    working front end

--- front_end/static/scripts.js
+++ front_end/static/scripts.js
@@ -1,17 +1,42 @@
+// Running Loop keeping alive connection with back-end
 
 document.addEventListener('DOMContentLoaded', function () {
-  function keep_alive_server() {
-    fetch(document.location + "random-endpoint", {
-      method: 'GET',
-      cache: 'no-cache'
-    })
-      .then(res => { })
-      .catch(err => { })
-  }
+    keep_alive_server()
+    try {setInterval(keep_alive_server, 5 * 1000)()
+    } catch (error) {}
+});
 
-  try {
-    setInterval(keep_alive_server, 3 * 1000)()
-  } catch (error) {
-    // doesn't matter handled by middleware
-  }
-})
+function keep_alive_server() {
+    fetch(document.location + "keep-alive/?alive=true", {
+        method: 'GET',
+        cache: 'no-cache'
+    })
+        .then(resp => resp.json())
+        .then(data => {
+            document.getElementById("heightText").innerHTML = data.height
+            document.getElementById("algoIcon").innerHTML = data.algo.icon
+            document.getElementById("algoText").innerHTML = data.algo.text
+            document.getElementById("deltaText").innerHTML = data.delta
+
+        })
+        .catch(error => {console.error(error)});
+    }
+
+
+function apiCall(body, query, cbFieldId, method='POST') {
+    fetch(query, {
+        method: method,
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body),
+    })
+        .then(resp => resp.text())  // or, resp.json(), etc.
+        .then(data => {
+            document.getElementById(cbFieldId).innerHTML = data
+        })
+        .catch(error => {
+            console.error(error)
+        });
+}
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import time
 from email.policy import default
 
+import requests
 from flaskwebgui import FlaskUI
 from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
@@ -39,9 +41,41 @@ async def calculate(parser: Parser):
     blockchain = Blockchain(**provider.BlockchainData().get())
     calc = Calculator(rig=rig, blockchain=blockchain, currency=currency, pool_fee=parser.pool_fee)
     print(calc.get_report(as_dict=False).formatted())
-    return calc.get_report()
+    return calc.get_report(as_dict=False).formatted()
 
+@app.get('/keep-alive/')
+async def keep_alive():
+    """
+    Front-end needs to ping this endpoint to keep connection,
+    we will use it also as front-end dashboard update thread.
+    """
+    request = requests.get("https://epic-radar.com/api/explorer/blocks/")
+    if request.status_code in [200, 2001]:
+        block = request.json()['results'][0]
 
+        algo = block['algo']
+        height = block['height']
+        avg_time = block['avg_time']
+        timestamp = block['timestamp']
+        total_diffs = block['target_diffs']
+        network_hashrate = block['network_hashrate']
+
+        last_block_delta = time.time() - timestamp
+
+        if last_block_delta < 60:
+            delta = f'< minute ago'
+        else:
+            delta = f"{int(last_block_delta / 60)} minute{'s' if last_block_delta / 60 > 2 else ''} ago"
+
+        algo_icons = {
+            'randomx': {'icon': '<span class="material-icons">memory</span>', 'text': 'RandomX'},
+            'progpow': {'icon': '<span class="material-icons">sports_esports</span>', 'text': 'ProgPoW'},
+            'cuckoo': {'icon': '<span class="material-icons">dns</span> ', 'text': 'Cuckoo'},
+                        }
+
+        return {'delta': delta, 'height': height, 'algo': algo_icons[algo]}
+
+    return {'----'}
 # @app.post("/login/", response_class=HTMLResponse)
 # async def login(request: Request, password: str = Form(default=None)):
 #     print(GPUtil.showUtilization())
